fefd8d4521593cee27b66d6db94c2654d604176e
Mike-chege <mikechege93@gmail.com>
2023-12-11 09:21:33 +0000

    Creating asyncronous coroutine / Fixed pep8

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -10,7 +10,8 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Asynchronous coroutine that waits for a random delay between 0 and max_delay seconds.
+    Asynchronous coroutine that waits for a random delay between
+    0 and max_delay seconds.
 
     Parameters:
     - max_delay (int): The maximum delay in seconds. Defaults to 10.
