3036555e6d35f4139399a51bf9eaab041b4e8b82
Mike-chege <mikechege93@gmail.com>
2023-12-12 07:04:41 +0000

    Yielding a random number between 0 and 10 / Fixed typos

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,12 +4,12 @@ A coroutine called async_generator
 That takes no arguments
 """
 
-
+from typing import Generator
 import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronous coroutine that yields a random number between 0 and 10
     After waiting for 1 second,
@@ -17,4 +17,4 @@ async def async_generator():
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
