1d721e4ca40c2fd4f8647f903f69a22022c63a94
Mike-chege <mikechege93@gmail.com>
2024-01-06 05:35:30 +0000

    Teast for GithubOrgClient class

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,17 +3,17 @@
 Unittests
 """
 
-import unittest
-from unittest.mock import patch, Mock, PropertyMock
+from unittest import TestCase, mock
+from unittest.mock import patch, Mock
 from parameterized import parameterized
 
+import client
 from client import GithubOrgClient
 
 
-class TestGithubOrgClient(unittest.TestCase):
+class TestGithubOrgClient(TestCase):
     """
-    Class for testing
-    GithubOrgClient
+    Class for testing GithubOrgClient
     """
 
     @parameterized.expand([
@@ -22,24 +22,9 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @patch('client.get_json')
     def test_org(self, org_name, mock_json):
-        """
-        Test method
-        Returns correct output
-        """
-        endpoint = 'https://api.github.com/orgs/{}'.format(org_name)
-        spec = GithubOrgClient(data)
-        spec.org()
-        mock_json.assert_called_once_with(endpoint)
-
-    @parameterized.expand([
-        ("random-url", {'repos_url': 'http://some_url.com'})
-    ])
-    def test_public_repos_url(self, name, result):
-        """
-        Test method
-        Returns correct output
-        """
-        with patch('client.GithubOrgClient.org',
-                   PropertyMock(return_value=result)):
-            response = GithubOrgClient(name)._public_repos_url
-            self.assertEqual(response, result.get('repos_url'))
+        """ Test method returns correct output """
+        gc = GithubOrgClient(org_name)
+        gc.org()
+        mock_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+            )
